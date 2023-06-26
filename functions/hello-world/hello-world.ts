11303fbd1a0afafa51783ce9c8a26295d38708e6
jcoegom@gmail.com <jcoegom@gmail.com>
2023-06-26 19:42:33 +0200

    tracking lambda func

--- functions/hello-world/hello-world.ts
+++ functions/hello-world/hello-world.ts
@@ -1,6 +1,7 @@
 import { Handler } from "@netlify/functions";
 
 export const handler: Handler = async (event, context) => {
+  console.log("pasaaaaaaaaaaaa");
   return {
     statusCode: 200,
     body: JSON.stringify({
