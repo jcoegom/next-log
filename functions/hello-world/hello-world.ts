28443d4fb90f98314373cfbd4e5ffd6a76f9eb31
jcoegom@gmail.com <jcoegom@gmail.com>
2023-06-26 20:04:01 +0200

    change function error

--- functions/hello-world/hello-world.ts
+++ functions/hello-world/hello-world.ts
@@ -2,6 +2,7 @@ import { Handler } from "@netlify/functions";
 
 export const handler: Handler = async (event, context) => {
   console.log("pasaaaaaaaaaaaa");
+  console.error("erorrrrrrrrrrrrrrrrrr");
   console.log(event);
   console.log(context);
   console.log("enddddddddddddd");
