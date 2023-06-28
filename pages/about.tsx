3aade5b65df6369ee6014af5a5779feda70de98c
jcoegom@gmail.com <jcoegom@gmail.com>
2023-06-28 18:53:44 +0200

    add getstaticprops

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,31 @@
 import { useRouter } from "next/router";
 
-const About = ({ name }: { name: string }) => {
+const About = ({ name, subname }: { name: string; subname: string }) => {
   const router = useRouter();
   const { value } = router.query;
   if (value == "err") throw new Error("Value error");
-  return <div>About</div>;
+  return (
+    <div>
+      About
+      <br />
+      name:{name}
+      <br />
+      subname:{subname}
+    </div>
+  );
 };
+
+export async function getStaticProps() {
+  // Fetch data from an API or perform any async operations
+
+  return {
+    props: {
+      name: "Jose",
+      subname: "Props",
+    },
+    // Re-generate this page every 10 seconds (for demonstration purposes)
+    revalidate: 10,
+  };
+}
 
 export default About;
