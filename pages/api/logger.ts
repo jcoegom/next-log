c915274f979c98a3034b44ad3e121b375449cddb
jcoegom@gmail.com <jcoegom@gmail.com>
2023-07-03 18:55:58 +0200

    change logger function. Name was changed

--- pages/api/logger.ts
+++ pages/api/logger.ts
@@ -10,5 +10,5 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
   console.log("pasaaaaaa Logger", req);
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name: "John Doe 1" });
 }
